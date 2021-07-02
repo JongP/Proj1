df7bf1105e9653c29a4fb9f07c445f8d6d32969b
jhtommyjr1 <tommyjr1@naver.com>
2021-07-02 22:01:51 +0900

    DB 제작

---
+++ app/src/main/java/com/example/viewpagerexample/Room/UserDao_wallet.java
@@ -0,0 +1,44 @@
+package com.example.viewpagerexample.Room;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface UserDao_wallet {
+
+    @Insert
+    void insert(User_wallet user);
+
+    @Update
+    void update(User_wallet user);
+
+
+
+
+    @Query("UPDATE Wallet SET quantity =:t, value =:s WHERE symbol = :p")
+    void update(int t, Double s, String p);
+
+    @Query("SELECT quantity FROM Wallet WHERE symbol =:p")
+    int getQuantity(String p);
+
+    @Query("SELECT * FROM Wallet WHERE symbol =:p")
+    User_wallet getWallet(String p);
+
+
+    @Delete
+    void delete(User_wallet user);
+
+    @Query("SELECT * FROM  Wallet")
+    List<User_wallet> getAll();
+
+    @Query("DELETE FROM Wallet")
+    void deleteAll();
+
+    @Query("SELECT COUNT(*) as cnt FROM Wallet")
+    int getDataCount();
+}
---
+++ app/src/main/java/com/example/viewpagerexample/Room/User_wallet.java
@@ -0,0 +1,53 @@
+package com.example.viewpagerexample.Room;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Wallet")
+public class User_wallet {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "symbol") //컬럼명 변수명과 다르게 사용 가능
+    private String sym;
+
+    @ColumnInfo(name = "quantity") //컬럼명 변수명과 다르게 사용 가능
+    private int quan;
+
+    @ColumnInfo(name = "value") //컬럼명 변수명과 다르게 사용 가능
+    private Double val;
+
+    public User_wallet(String sym, int quan, Double val) {
+        this.sym = sym;
+        this.quan = quan;
+        this.val = val;
+    }
+
+
+    public String getSym() {
+        return sym;
+    }
+
+    public void setSym(String sym) {
+        this.sym = sym;
+    }
+
+    public int getQuan() {
+        return quan;
+    }
+
+    public void setQuan(int quan) {
+        this.quan = quan;
+    }
+
+    public Double getVal() {
+        return val;
+    }
+
+    public void setVal(Double val) {
+        this.val = val;
+    }
+}
+
