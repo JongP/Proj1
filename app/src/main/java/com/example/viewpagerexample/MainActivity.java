ff2f9faafe85f1d5d8579645fb3558e66eaa1da4
JongP <jong.p@kaist.ac.kr>
2021-07-05 11:01:10 +0900

    Permission requirement

--- app/src/main/java/com/example/viewpagerexample/MainActivity.java
+++ app/src/main/java/com/example/viewpagerexample/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.viewpagerexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.viewpagerexample.adapters.ViewPagerAdaptor;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
+import org.jetbrains.annotations.NotNull;
+
 public class MainActivity extends AppCompatActivity {
 
     private FragmentStateAdapter fragmentPagerAdapter;
+    int nCurrentPermission =0;
+    static final int PERMISSIONS_REQUEST = 1004;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        OnCheckPermission();
 
         //뷰페이저 세팅
         ViewPager2 viewPager = findViewById(R.id.viewPager);
@@ -44,7 +54,35 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         ) ).attach();
+    }
 
 
+    public void OnCheckPermission(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED
+        ||ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+        ||ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_CONTACTS)
+            ||ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)
+            ||ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CALL_PHONE)){
+                Toast.makeText(this,"Permission is required",Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_CONTACTS,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.CALL_PHONE}
+            ,PERMISSIONS_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults); //check
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission is granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission is retrieved", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
     }
 }
